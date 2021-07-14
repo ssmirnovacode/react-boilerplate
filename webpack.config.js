f0b3f69ac10eb129c693597478000806b67b2357
ssmirnova <svetlana.smirnova213@gmail.com>
2021-07-14 19:56:51 +0200

    build and prod scripts enabled, css -> main.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,81 @@
 const HtmlWebpackPlugin =require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
+module.exports = (env, options) => {
 
-    module: {
-        rules: [
-            // babel-loader to work with React ( processing JSX )
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader' // shorter syntax
-            },
-            //images loader
-            {
-                test: /\.(png|jpg|jpeg|gif|ico)$/,  //choosing files to be processed by this loader
-                use: [ // array, each elem of which describes a loader and its config
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images', // to save processed images to this folder
-                            name: '[name]-[sha1:hash:7].[ext]' //original name + '-' +  first 7 symbols of hashed name
+    const { mode = 'development'} = options; // default
+
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
+
+    return {
+        mode: isProd ? 'production' : isDev && 'development', // else it will be false and settings won´t be applied
+
+        module: {
+            rules: [
+                // babel-loader to work with React ( processing JSX )
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader' // shorter syntax
+                },
+                //images loader
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,  //choosing files to be processed by this loader
+                    use: [ // array, each elem of which describes a loader and its config
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images', // to save processed images to this folder
+                                name: '[name]-[sha1:hash:7].[ext]' //original name + '-' +  first 7 symbols of hashed name
+                            }
                         }
-                    }
-                    //there might be more loaders in this array
-                ]
-            },
-            //Fonts loader
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,  //fonts extensions
-                use: [ 
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts', 
-                            name: '[name]-[sha1:hash:7].[ext]'
+                        //there might be more loaders in this array
+                    ]
+                },
+                //Fonts loader
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,  //fonts extensions
+                    use: [ 
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts', 
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
                         }
-                    }
-                    
-                ]
-            },
-            // CSS loader           
-            {
-                test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'] // shorter syntax
-            },
-            // SCSS processing
-            {
-                test: /\.(scss)$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // shorter syntax
-            }
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My customized React app',
-            buildTime: new Date().toString(),
-            template: 'public/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main.css' // specifying the name of output file 
-            /* main.css is default, alternative for testing 'main-[hash:8].css':
-            (browser is caching css, and if the name is the same in each build, it might not update it), 
-            therefore we make it unique for each build with [hash:8] */
-        })
-    ],
-
-    devServer: {
-        open: true, // automatically opening the browser after build
-        hot: true,
-        contentBase: './dist'
+                        
+                    ]
+                },
+                // CSS loader           
+                {
+                    test: /\.(css)$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'] // shorter syntax
+                },
+                // SCSS processing
+                {
+                    test: /\.(scss)$/,
+                    use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // shorter syntax
+                }
+            ]
+        },
+    
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'My customized React app',
+                buildTime: new Date().toString(),
+                template: 'public/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'main.css' // specifying the name of output file 
+                /* main.css is default, alternative foe testing 'main-[hash:8].css':
+                (browser is caching css, and if the name is the same in each build, it might not update it), 
+                therefore we make it unique for each build with [hash:8] */
+            })
+        ],
+    
+        devServer: {
+            open: true // automatically opening the browser after build
+        }
     }
 };
