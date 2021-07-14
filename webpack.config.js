de886a60e1a6ef6da9e8e1e2944b200874bdd6b7
ssmirnova <svetlana.smirnova213@gmail.com>
2021-07-14 15:16:51 +0200

    File-loader installed and enabled

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    mode: "development"
+    mode: "development",
+
+    module: {
+        rules: [
+            {
+                test: /\.png$/,  //choosing files to be processed by this loader
+                use: [ // array, each elem of which describes a loader and its config
+                    {
+                        loader: 'file-loader'
+                    }
+                    //there might be more loaders in this array
+                ]
+            }
+        ]
+    }
 };
